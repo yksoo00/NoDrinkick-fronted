4364accd699b0df8f70c3dfcf4d457506770265b
yksoo <yksoo00@naver.com>
2024-04-26 01:55:09 +0900

    지훈이꺼까지 추가

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,12 @@ import '../App.css';
 import AImage from '../images/Logo.png';
 import BImage from '../images/Logo.png';
 import BackImage from '../images/Main.png';
-import '../css/home.css';
+import '../css/home.css'; 
 
 function Home() {
   const [showImage, setShowImage] = useState(true);
   const [showNodeKick, setShowNodeKick] = useState(true);
-  const [showBackgroundShape, setShowBackgroundShape] = useState(false);
+  const [showBackgroundShape, setShowBackgroundShape] = useState(false); 
   const [showBImage, setShowBImage] = useState(false);
 
   useEffect(() => {
@@ -43,14 +43,14 @@ function Home() {
 
 
   return (
-
-
+    
+    
     <div className="container text-center" style={{ maxWidth: '1500px', position: 'relative' }}>
       {showBackgroundShape && <div className="background-shape"></div>}
       <div className="image-container" style={{ position: 'absolute', top: '200px', left: '50%', transform: 'translateX(-50%)' }}>
-      {showImage && <img src={AImage} alt="A" style={{ width: '200px', animation: 'loadEffect1 0.7s ease-in-out' }} />}
+      {showImage && <img src={AImage} alt="A" className="Logo-A" />}
         {showNodeKick && (
-           <div className="logo" style={{ padding: '21px', borderRadius: '10px', marginTop: '-30px' }}>
+           <div className="logo" style={{ padding: '30px', borderRadius: '10px', marginTop: '-30px' }}>
            노드링킥
          </div>
         )}
@@ -73,8 +73,8 @@ function Home() {
     </div>
     <hr className="my-4" />
     <div className="text-container">
-    {showBImage && <img src={BImage} alt="B" style={{ position: 'absolute', width:'50px', height:'50px', top: '350px', left:'62%'  }} />}
-    <h2 className="text">노드링킥은 당신의 생명을 책임집니다</h2>
+    {showBImage && <img src={BImage} alt="B" className="Logo-B" style={{ position: 'absolute', width:'50px', height:'50px', top: '350px', left:'62%'  }} />}
+    <h2 className="text">노드링킥은 당신의 생명을 책임집니다 </h2>
         </div>
       </div>
       {showBackgroundShape && <img src={BackImage} alt="C" className="back-image" />}
