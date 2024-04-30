7569d8c751e9a481b8c68a6e13bcaa59ee6962a6
kjskjs0225 <132901377+kjskjs0225@users.noreply.github.com>
2024-04-30 20:17:16 +0900

    수정

--- src/pages/addEmergency.js
+++ src/pages/addEmergency.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import '../styles/addEmergency.css';
 import axios from 'axios';
+import BackImage from '../assets/Main2.png'; // 이미지를 가져옵니다.
 
 function EmergencyContactForm() {
     const [contactInfo, setContactInfo] = useState({
@@ -32,24 +34,26 @@ function EmergencyContactForm() {
     
     
     return (
-        <div>
-            <h2>비상연락망 추가</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>이름:</label>
-                    <input type="text" name="name" value={contactInfo.name} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>전화번호:</label>
-                    <input type="text" name="phoneNum" value={contactInfo.phoneNum} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>음성 메시지:</label>
-                    <input type="text" name="voiceMessage" value={contactInfo.voiceMessage} onChange={handleChange} />
-                </div>
-               
-                <button type="submit">추가하기</button>
-            </form>
+        <div className="background1">
+            <div className="container1">
+                <h3>비상연락망 추가</h3>
+                <form1 onSubmit={handleSubmit}>
+                    <div>
+                        <label1>이름:</label1>
+                        <input type="text" name="name" value={contactInfo.name} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label1>전화번호:</label1>
+                        <input type="text" name="phoneNum" value={contactInfo.phoneNum} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label1>메시지:</label1>
+                        <input type="text" name="voiceMessage" value={contactInfo.voiceMessage} onChange={handleChange} />
+                    </div>
+                
+                    <button1 type="submit">추가하기</button1>
+                </form1>
+            </div>
         </div>
     );
 }
