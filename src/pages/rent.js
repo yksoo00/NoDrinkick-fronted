bd0206626d180d79bb97dab7a60dde7c7a890038
wlgns5041 <wlgns5041@naver.com>
2024-05-22 09:05:29 +0900

    배포 및 미디어쿼리 수정2

--- src/pages/rent.js
+++ src/pages/rent.js
@@ -16,27 +16,28 @@ export default function Rent({ open, onClose }) {
         onOpen={() => {}}
         sx={{ 
           '& .MuiDrawer-paper': { 
-            height: 700,
+            height: 450,
             borderTopLeftRadius: '20px', 
             borderTopRightRadius: '20px', 
 
            }, }}
       >
+
         <div className="Box-1">
          <img className="KickBoardImage" src={KickBoardImage} alt="KickBoardImage" />
             <h2 className="KickBoardName">킥보드이름</h2>
               </div>
+                <div className="Test-Both">
+                    <div className="Face">
+                      <h2 className="Face-Text">얼굴인식</h2>
+                        <label className="Face-Test">인증완료</label>
+                    </div>
 
-          <div className="Face">
-           <h2 className="Face-Text">얼굴인식</h2>
-             <label className="Face-Test">인증완료</label>
-               </div>
-
-
-         <div className="Alchol">
-            <h2 className="Alchol-Text">음주측정</h2>
-            <label className="Alchol-Test">인증완료</label>
-              </div>
+                    <div className="Alchol">
+                      <h2 className="Alchol-Text">음주측정</h2>
+                        <label className="Alchol-Test">인증완료</label>
+                    </div>
+                </div>
 
               <Button variant="contained" style={{left:'5%', backgroundColor: '#2d2c28', color: '#ffffff', height:'10vh' ,width: '90vw' }} >
           <Typography variant="h6" sx={{fontFamily: 'Pretendard-Bold' }}>대여하기</Typography>
