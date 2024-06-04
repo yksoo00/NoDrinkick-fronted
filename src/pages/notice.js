d8137de0bd76cb6841ad8e9633da33dde8530ced
Hyeongseok0410 <taeinzz0410@gmail.com>
2024-06-04 15:49:41 +0900

    공지사항 api분리

--- src/pages/notice.js
+++ src/pages/notice.js
@@ -12,25 +12,16 @@ import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box'; 
-
 import { removeToken } from '../services/loginService';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // 아이콘 정의
-import { faUser } from '@fortawesome/free-solid-svg-icons'; //마이페이지 아이콘
-import { faHouse } from '@fortawesome/free-solid-svg-icons'; //마이페이지 아이콘
-import { faClipboard } from '@fortawesome/free-solid-svg-icons'; //이용내역 아이콘
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons'; //비상연락망 추가 아이콘
-import { faAddressBook } from '@fortawesome/free-solid-svg-icons'; //비상연락망 목록 아이콘
-import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'; //이용약관 아이콘
-import { faBell } from '@fortawesome/free-solid-svg-icons'; //이용약관 아이콘
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faHouse, faClipboard, faUserPlus, faAddressBook, faCircleInfo, faBell } from '@fortawesome/free-solid-svg-icons';
+import { fetchNotices, createNotice } from '../services/noticesService';
 
 const Notices = () => {
   const [open, setOpen] = useState(false); 
   const [notices, setNotices] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [users, setUsers] = useState([]);
   const [newNotice, setNewNotice] = useState({ title: '', content: '' });
   const [darkModeEnabled, setDarkModeEnabled] = useState(
     localStorage.getItem('darkModeEnabled') === 'true'
@@ -46,22 +37,26 @@ const Notices = () => {
     localStorage.setItem('darkModeEnabled', darkModeEnabled);
   }, [darkModeEnabled]);
 
-  const fetchNotices = async () => {
-    const response = await axios.get('http://localhost:8080/notices');
-    setNotices(response.data);
+  const loadNotices = async () => {
+    try {
+        const notices = await fetchNotices();
+        setNotices(notices);
+    } catch (error) {
+        console.error('Failed to load notices:', error);
+    }
   };
 
   const handleCreate = async () => {
     if (newNotice.title && newNotice.content) {
-      await createNotice(newNotice);
+        try {
+            await createNotice(newNotice);
+            loadNotices();
+            setIsModalOpen(false);
+            setNewNotice({ title: '', content: '' });
+        } catch (error) {
+            console.error('Failed to create notice:', error);
+        }
     }
-  };
-
-  const createNotice = async (notice) => {
-    await axios.post('http://localhost:8080/notices', notice);
-    fetchNotices();
-    setIsModalOpen(false);
-    setNewNotice({ title: '', content: '' });
   };
 
   const handleTitleClick = (noticeId) => {
@@ -92,8 +87,7 @@ const Notices = () => {
         break;
       case '이용기록':
         path = '/UserRecord';
-        break;                         
-        
+        break;
       default:
         path = '/';
         break;
@@ -102,7 +96,7 @@ const Notices = () => {
   };
 
   useEffect(() => {
-    fetchNotices();
+    loadNotices();
   }, []);
 
   useEffect(() => {
@@ -117,39 +111,39 @@ const Notices = () => {
       backgroundColor: '#e8e8e8',
       minHeight: '100vh', 
       display: 'flex',
-  }}>
-    <AppBar position="fixed" sx={{
-      zIndex: 9999,
-      backgroundColor: darkModeEnabled ? '#F2F2F2' : '#2d2c28',
-      transition: 'background-color 0.5s ease'
     }}>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={toggleDrawer} sx={{ mr: 2,  color: darkModeEnabled ? '#2d2c28' : '#FFFFFF'}}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" sx={{fontSize: 12, fontFamily: 'Pretendard-Bold', textAlign: 'center', color: darkModeEnabled ? '#2d2c28' : '#FFFFFF', transition: 'color 0.5s ease'}} component="div"> 공지사항 </Typography>
-        </Box>
-        <Box />
-        <Box>
-        <IconButton 
-        color="inherit" 
-        onClick={() => history.push('/main')}
-        style={{ color: darkModeEnabled ? '#000000' : '#ffffff' }}
+      <AppBar position="fixed" sx={{
+        zIndex: 9999,
+        backgroundColor: darkModeEnabled ? '#F2F2F2' : '#2d2c28',
+        transition: 'background-color 0.5s ease'
+      }}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={toggleDrawer} sx={{ mr: 2, color: darkModeEnabled ? '#2d2c28' : '#FFFFFF'}}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" sx={{fontSize: 12, fontFamily: 'Pretendard-Bold', textAlign: 'center', color: darkModeEnabled ? '#2d2c28' : '#FFFFFF', transition: 'color 0.5s ease'}} component="div"> 공지사항 </Typography>
+          </Box>
+          <Box />
+          <Box>
+            <IconButton 
+              color="inherit" 
+              onClick={() => history.push('/main')}
+              style={{ color: darkModeEnabled ? '#000000' : '#ffffff' }}
+            >
+              <FontAwesomeIcon icon={faHouse} />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        sx={{zIndex: 999}}
       >
-        <FontAwesomeIcon icon={faHouse} />
-      </IconButton>
-                  </Box>
-      </Toolbar>
-    </AppBar>
-    <Drawer
-      anchor="left"
-      open={open}
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      sx={{zIndex: 999}}
-    >
-      <List>
+        <List>
           {['마이페이지', '이용기록', 'SOS 추가', 'SOS 목록', '이용약관', '공지사항'].map((text, index) => (
             <ListItem
               button
@@ -170,35 +164,32 @@ const Notices = () => {
               </Typography>
             </ListItem>
           ))}
-      
         </List>
       </Drawer>
       <div>
-      <button className="add-button" onClick={() => setIsModalOpen(true)}>공지사항 추가하기</button>
-      
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <input 
-              className="notice-title"
-              type="text"
-              placeholder="제목" 
-              value={newNotice.title} 
-              onChange={(e) => setNewNotice({ ...newNotice, title: e.target.value })}
-            />
-            <input 
-              className="notice-text"
-              type="text"
-              placeholder="내용" 
-              value={newNotice.content} 
-              onChange={(e) => setNewNotice({ ...newNotice, content: e.target.value })}
-            />
+        <button className="add-button" onClick={() => setIsModalOpen(true)}>공지사항 추가하기</button>
+        {isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <input 
+                className="notice-title"
+                type="text"
+                placeholder="제목" 
+                value={newNotice.title} 
+                onChange={(e) => setNewNotice({ ...newNotice, title: e.target.value })}
+              />
+              <input 
+                className="notice-text"
+                type="text"
+                placeholder="내용" 
+                value={newNotice.content} 
+                onChange={(e) => setNewNotice({ ...newNotice, content: e.target.value })}
+              />
+            </div>
+            <button className="notice-addbutton" onClick={handleCreate}>추가</button>
           </div>
-          <button className="notice-addbutton" onClick={handleCreate}>추가</button>
-        </div>
-      )}
-
-<div className="Full-notice">
+        )}
+        <div className="Full-notice">
           {notices.map((notice, index) => (
             <div className="notice-button" key={notice.id} onClick={() => handleTitleClick(notice.noticeId)}>
               <span className="notice-number">{index + 1}</span>
