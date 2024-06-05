737a0e53f6510e6e42c82ae886139c6f7bbae942
Hyeongseok0410 <taeinzz0410@gmail.com>
2024-06-05 17:51:41 +0900

    대여하기 버튼 라이센스값에따라 활성화

--- src/component/test.js
+++ src/component/test.js
@@ -1,21 +1,163 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { sendEmergencyMessage } from '../services/rent';
+import { fetchUserData, fetchUserProfileImage } from '../services/userService';
 
-function Test({ isOpen, onClose, onStartTest }) {
-  
+function Test({ isOpen, onClose, setAuthState }) {
+  const [showMessage, setShowMessage] = useState(false);
+  const [countdown, setCountdown] = useState(3);
+  const [showBreathMessage, setShowBreathMessage] = useState(false);
+  const [showFaceRecognitionMessage, setShowFaceRecognitionMessage] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [returnAlc, setReturnAlc] = useState(false);
+  const [returnFace, setReturnFace] = useState(false);
+  const [Alc, setAlc] = useState(false);
+  const [face, setFace] = useState(false);
+  const [faceMessage, setfaceMessage] = useState(false);
+  const [memberInfo, setMemberInfo] = useState({});
+  const [profileImageUrl, setProfileImageUrl] = useState('');
+  const [wsMessages, setWsMessages] = useState([]);
+  const [ws, setWs] = useState(null);
+  const [buttonText, setButtonText] = useState("인증시작");
+
+  const handleWebSocketMessage = (data) => {
+    console.log("서버로부터 받은 메시지:", data);
+    setWsMessages(prevMessages => [...prevMessages, data]);
+
+    switch (data) {
+      case "33":
+        sendMessageToAll();
+        break;
+      case "5":
+        setAlc(true);
+        break;
+      case "2":
+        setFace(true);
+        break;
+      case "20":
+        setReturnAlc(true);
+        break;
+      case "50":
+        setReturnFace(true);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const fetchMemberInfoAndConnect = async () => {
+    try {
+      const memberInfo = await fetchUserData();
+      setMemberInfo(memberInfo);
+      const username = encodeURIComponent(memberInfo.username);
+      const wsUrl = `ws://192.168.15.197:8765/ws?username=${username}`;
+      const newWs = new WebSocket(wsUrl);
+      newWs.onopen = () => console.log("WebSocket 연결 성공");
+      newWs.onmessage = (event) => handleWebSocketMessage(event.data);
+
+      setWs(newWs);
+
+      // Fetch profile image URL
+      const imageUrl = await fetchUserProfileImage(memberInfo.memberId);
+      setProfileImageUrl(imageUrl);
+    } catch (error) {
+      console.error('API 호출 또는 WebSocket 연결 중 오류 발생:', error);
+    }
+  };
+
+  const sendMessageToAll = async () => {
+    try {
+      const response = await sendEmergencyMessage();
+      alert(response);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleStartAuth = () => {
+    if (buttonText === "확인") {
+      onClose(); // 확인 버튼일 때는 모달을 닫음
+    } else {
+      sendNumber(); // 다른 동작 수행
+      setShowMessage(true);
+      setCountdown(3);
+      setIsButtonDisabled(true);
+    }
+  };
+
+  const sendNumber = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send('1');
+    } else {
+      console.error('WebSocket이 연결 상태가 아닙니다.');
+    }
+  };
+
+  useEffect(() => {
+    let timer;
+    if (showMessage && countdown > 0) {
+      timer = setTimeout(() => setCountdown(prev => prev - 1), 1000);
+    } else if (showMessage && countdown === 0) {
+      setShowMessage(false);
+      setShowBreathMessage(true);
+      setCountdown(5);
+    } else if (showBreathMessage && countdown > 0) {
+      timer = setTimeout(() => setCountdown(prev => prev - 1), 1000);
+    } else if (showBreathMessage && countdown === 0) {
+      setShowBreathMessage(false);
+      setCountdown(18); // 얼굴 인식을 위한 타이머 설정
+      setShowFaceRecognitionMessage(true);
+    } else if (showFaceRecognitionMessage && countdown > 0) {
+      timer = setTimeout(() => setCountdown(prev => prev - 1), 1000);
+    } else if (showFaceRecognitionMessage && countdown === 0) {
+      setShowFaceRecognitionMessage(false);
+      setIsButtonDisabled(false);
+    } else if (faceMessage && countdown > 0) {
+      timer = setTimeout(() => setCountdown(prev => prev - 1), 1000);
+    } else if (faceMessage && countdown === 0) {
+      setAuthState(true); // 인증 성공 상태 변경
+      setfaceMessage(false);
+      setIsButtonDisabled(false);
+    }
+
+    return () => clearTimeout(timer);
+  }, [showMessage, showBreathMessage, showFaceRecognitionMessage, countdown, Alc, face]);
+
+  useEffect(() => {
+    fetchMemberInfoAndConnect();
+  }, []);
+
+  useEffect(() => {
+    if (Alc) {
+      const timer = setTimeout(() => {
+        setShowFaceRecognitionMessage(true);
+        setCountdown(18);
+        setAlc(false);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [Alc]);
+
+  useEffect(() => {
+    if (face) {
+      setShowFaceRecognitionMessage(false); // 얼굴 인식 화면을 숨김
+      setfaceMessage(true);
+      setFace(false);
+      setButtonText("확인");
+      setIsButtonDisabled(false);
+    }
+  }, [face]);
+
   return (
     <Modal
       open={isOpen}
       onClose={onClose}
       aria-labelledby="auth-modal-title"
       aria-describedby="auth-modal-description"
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
+      style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
     >
       <div style={{
         backgroundColor: '#ffffff',
@@ -25,17 +167,79 @@ function Test({ isOpen, onClose, onStartTest }) {
         height: '50%',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+        position: 'relative'
       }}>
         <Typography variant="h5" id="auth-modal-title" sx={{ fontSize: 20, fontFamily: 'Pretendard-Bold', marginBottom: '20px', textAlign: 'center' }}>
-          인증을 진행해주세요
+          인증을 진행 중
         </Typography>
-        <Button variant="contained" onClick={() => onStartTest("5")} sx={{ backgroundColor: '#2d2c28', fontSize: 12, fontFamily: 'Pretendard-Bold', width: '50%', marginTop: 'auto' }}>
-  인증 시작
-</Button>
+        {showMessage && (
+          <>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 14, fontFamily: 'Pretendard-Bold', top: '120px', textAlign: 'center' }}>
+              <span style={{ color: 'red' }}>3초</span> 후에 음주센서에 <span style={{ color: 'red' }}>5초</span> 동안 입김을 불어주세요
+            </Typography>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 100, fontFamily: 'Pretendard-Bold', top: '150px', textAlign: 'center' }}>
+              {countdown}
+            </Typography>
+          </>
+        )}
+        {showBreathMessage && (
+          <>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 14, fontFamily: 'Pretendard-Bold', top: '120px', textAlign: 'center' }}>
+              <span style={{ color: 'red' }}>5초</span> 동안 <span style={{ color: 'red' }}>음주 측정</span>을 진행해주세요
+            </Typography>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 100, fontFamily: 'Pretendard-Bold', top: '150px', textAlign: 'center' }}>
+              {countdown}
+            </Typography>
+          </>
+        )}
+        {Alc && !showFaceRecognitionMessage && (
+          <>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 14, fontFamily: 'Pretendard-Bold', top: '200px', textAlign: 'center' }}>
+              <span style={{ color: 'green' }}>음주 측정에 성공했습니다. <br></br> 잠시 후에 얼굴 인식을 시작합니다.</span>
+            </Typography>
+          </>
+        )}
+        {showFaceRecognitionMessage && (
+          <>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 14, fontFamily: 'Pretendard-Bold', top: '80px', textAlign: 'center' }}>
+              <span style={{ color: 'red' }}>얼굴 인식을 진행 중입니다.<br></br> 카메라에 얼굴을 인식해주세요</span>
+            </Typography>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 100, fontFamily: 'Pretendard-Bold', top: '120px', textAlign: 'center' }}>
+              {countdown}
+            </Typography>
+            <img
+              src={profileImageUrl}
+              alt="회원 사진"
+              style={{
+                position: 'absolute',
+                width: '100px',
+                height: '100px',
+                borderRadius: '50%',
+                top: '250px'
+              }}
+            />
+          </>
+        )}
+        {faceMessage && (
+          <>
+            <Typography variant="body1" sx={{ position: 'absolute', fontSize: 14, fontFamily: 'Pretendard-Bold', top: '200px', textAlign: 'center' }}>
+              <span style={{ color: 'green' }}>얼굴 인식에 성공했습니다.</span>
+            </Typography>
+          </>
+        )}
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: '#2d2c28', fontSize: 12, fontFamily: 'Pretendard-Bold', width: '50%', marginTop: 'auto' }}
+          onClick={handleStartAuth}
+          disabled={isButtonDisabled}
+        >
+          {buttonText}
+        </Button>
       </div>
     </Modal>
   );
 }
 
 export default Test;
+
--- src/pages/rent.js
+++ src/pages/rent.js
@@ -1,70 +1,102 @@
+// Rent.js
+import React, { useState, useEffect } from 'react';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import Button from '@mui/material/Button'; 
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import KickBoardImage from '../assets/KickBoard.png';
 import '../styles/Rent.css';
-import Test from '../component/test'
-import React, { useState, useEffect } from 'react';
+import Test from '../component/test'; // 파일명 대소문자 주의
 import { faBell } from '@fortawesome/free-solid-svg-icons';
-import { faBeerMugEmpty } from '@fortawesome/free-solid-svg-icons';
-import { faFaceLaugh } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // 아이콘 정의
+import { faBeerMugEmpty, faFaceLaugh } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useMediaQuery } from '@mui/material';
-import { sendEmergencyMessage } from'../services/rent';
+import { sendEmergencyMessage } from '../services/rent';
 import { fetchUserData } from '../services/userService';
+
 function Rent({ open, onClose }) {
- 
-  let [Alc, setAlc] = useState(false); 
-  const [memberInfo, setMemberInfo] = useState({}); // 회원 정보 상태 추가
+  const [Alc, setAlc] = useState(false);
+  const [face, setFace] = useState(false);
+  const [memberInfo, setMemberInfo] = useState({});
   const [ws, setWs] = useState(null);
-  const [wsMessages, setWsMessages] = useState([]); // WebSocket 메시지 상태 추가
-  const [isTestOpen, setisTestOpen] = useState(false);
+  const [wsMessages, setWsMessages] = useState([]);
+  const [isTestOpen, setIsTestOpen] = useState(false);
+  const [returnAlc, setReturnAlc] = useState(false);
+  const [returnFace, setReturnFace] = useState(false);
+  const [AuthState, setAuthState] = useState(false); // 인증 상태를 관리합니다.
+  const [license, setLicense] = useState(false); // 라이센스 상태를 관리합니다.
   const isLargeScreen = useMediaQuery('(min-width:1440px) and (max-height:1440px)');
 
-  const handleStartTest = () => {
+  useEffect(() => {
+    const fetchMemberInfoAndConnect = async () => {
+      try {
+        const memberInfo = await fetchUserData();
+        setMemberInfo(memberInfo);
+        setLicense(memberInfo.license); // 사용자 라이센스 정보 설정
+        const username = encodeURIComponent(memberInfo.username);
+        const wsUrl = `ws://192.168.15.197:8765/ws?username=${username}`;
+        setWs(new WebSocket(wsUrl));
+      } catch (error) {
+        console.error('API 호출 또는 WebSocket 연결 중 오류 발생:', error);
+      }
+    };
+    
     fetchMemberInfoAndConnect();
-    setisTestOpen(false);
+  }, []);
+
+  useEffect(() => {
+    if (ws) {
+      ws.onopen = () => console.log("WebSocket 연결 성공");
+      ws.onmessage = (event) => handleWebSocketMessage(event.data);
+    }
+  }, [ws]);
+
+  const handleWebSocketMessage = (data) => {
+    console.log("서버로부터 받은 메시지:", data);
+    setWsMessages(prevMessages => [...prevMessages, data]);
+
+    switch(data) {
+      case "33":
+        sendMessageToAll();
+        break;
+      case "5":
+        setAlc(true);
+        break;
+      case "2":
+        setFace(true);
+        break;
+      case "20":
+        setReturnAlc(true);
+        break;
+      case "50":
+        setReturnFace(true);
+        break;
+      case "6":
+        sendNumber6(true);
+        break;
+      default:
+        break;
+    }
   };
 
+  const sendNumber6 = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send('6');
+    } else {
+      console.error('WebSocket이 연결 상태가 아닙니다.');
+      fetchMemberInfoAndConnect(); // WebSocket 연결이 없으면 다시 연결을 시도합니다.
+      ws.send('6');
+    }
+  };
 
   const fetchMemberInfoAndConnect = async () => {
     try {
       const memberInfo = await fetchUserData();
       setMemberInfo(memberInfo);
-
-      const username = encodeURIComponent(memberInfo.username); // 회원 정보 중 username 추출
-
-      const wsUrl = `ws://192.168.42.197:8765/ws?username=${username}`;
-      const newWs = new WebSocket(wsUrl);
-
-      newWs.onopen = () => console.log("WebSocket 연결 성공");
-
-      sendNumber();
-      newWs.onmessage = (event) => {
-        console.log("서버로부터 받은 메시지:", event.data);
-        setWsMessages(prevMessages => [...prevMessages, event.data]); // 새로운 메시지를 상태에 추가
-
-        if (event.data === "33") {
-          sendMessageToAll();
-        }
-
-        if(event.data === "5"){
-          Alc = true;
-          setAlc(Alc);
-        }
-      };
-
-      setWs(newWs);
+      const username = encodeURIComponent(memberInfo.username);
+      const wsUrl = `ws://192.168.15.197:8765/ws?username=${username}`;
+      setWs(new WebSocket(wsUrl));
     } catch (error) {
       console.error('API 호출 또는 WebSocket 연결 중 오류 발생:', error);
-    }
-  };
-
-  const sendNumber = () => {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      ws.send('1');
-    } else {
-      console.error('WebSocket이 연결 상태가 아닙니다.');
     }
   };
 
@@ -77,10 +109,19 @@ function Rent({ open, onClose }) {
     }
   };
 
+  const handleFaceButtonClick = () => {
+    fetchMemberInfoAndConnect();
+    setIsTestOpen(true);
+  };
+
+  const handleStartTest = () => {
+    fetchMemberInfoAndConnect();
+    setIsTestOpen(false);
+  };
+
   return (
     <div>
       <SwipeableDrawer
-        
         anchor="bottom"
         open={open}
         onClose={onClose}
@@ -89,50 +130,57 @@ function Rent({ open, onClose }) {
           '& .MuiDrawer-paper': { 
             height: 400,
             borderTopLeftRadius: '20px', 
-            borderTopRightRadius: '20px', 
-
-           }, }}
-      >
-        <div className='Rent-All'>
-        <div className="Box-1">
-         <img className="KickBoardImage" src={KickBoardImage} alt="KickBoardImage" />
-            <h2 className="KickBoardName">노드링킥 1</h2>
-              </div>
-                <div className="Test-Both">
-                <Button className="Face" style={{backgroundColor: '#e8e8e8', marginBottom:'20px', marginLeft: '20px', marginRight: '10px', padding:'20px' }} onClick={() => setisTestOpen(true)}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-            <FontAwesomeIcon icon={faFaceLaugh} style={{fontSize: '25px', color:'#000000', marginRight:'20px'}} />
-            <FontAwesomeIcon icon={faBeerMugEmpty} style={{fontSize: '25px', color:'#000000'}} />
-            </div>
-            <label className="Face-Test">{Alc ? '인증 성공' : '인증 실패'}</label> 
-          </Button>
-
-
-                    <Button className="Bell" style={{backgroundColor: '#e8e8e8' , marginBottom:'20px', marginRight: '20px', marginLeft: '10px', padding:'20px'}}>
-                    <FontAwesomeIcon icon={faBell} style={{fontSize: '25px', color:'#000000'}}></FontAwesomeIcon>
-                        <label className="Bell-Text">벨 울리기</label>
-                    </Button>
-                </div>
-                </div>
-                <Button
-        variant="contained"
-        style={{
-          left: isLargeScreen ? '27.5%' :'5%',
-          backgroundColor: '#2d2c28',
-          color: '#ffffff',
-          height: '10vh',
-          width: isLargeScreen ? '44.2vw' : '90vw'
+            borderTopRightRadius: '20px',
+          }
         }}
       >
-          <Typography variant="h6" sx={{fontFamily: 'Pretendard-Bold', fontSize : '20px' }}>대여하기</Typography>
+        <div className='Rent-All'>
+          <div className="Box-1">
+            <img className="KickBoardImage" src={KickBoardImage} alt="KickBoardImage" />
+            <h2 className="KickBoardName">노드링킥 1</h2>
+          </div>
+          <div className="Test-Both">
+            <Button 
+              className="Face" 
+              style={{backgroundColor: '#e8e8e8', marginBottom:'20px', marginLeft: '20px', marginRight: '10px', padding:'20px' }} 
+              onClick={handleFaceButtonClick}
+              disabled={!license || AuthState} // 라이센스가 false거나 AuthState가 true일 때 버튼 비활성화
+            >
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <FontAwesomeIcon icon={faFaceLaugh} style={{fontSize: '25px', color:'#000000', marginRight:'20px'}} />
+                <FontAwesomeIcon icon={faBeerMugEmpty} style={{fontSize: '25px', color:'#000000'}} />
+              </div>
+              <label className="Face-Test">{AuthState ? '대여 중 입니다' : '대여하기'}</label> 
+            </Button>
+            <Button className="Bell" style={{backgroundColor: '#e8e8e8' , marginBottom:'20px', marginRight: '20px', marginLeft: '10px', padding:'20px'}} onClick={sendNumber6}
+                          disabled={!license || AuthState}>
+                <FontAwesomeIcon icon={faBell} style={{fontSize: '25px', color:'#000000'}}></FontAwesomeIcon>
+                    <label className="Bell-Text">벨 울리기</label>
+              </Button>
+          </div>
+        </div>
+        <Button
+          variant="contained"
+          style={{
+            left: isLargeScreen ? '27.5%' :'5%',
+            backgroundColor: AuthState ? '#2d2c28' : '#e8e8e8', // AuthState에 따라 배경색 변경
+            color: '#ffffff',
+            height: '10vh',
+            width: isLargeScreen ? '44.2vw' : '90vw'
+          }}
+          disabled={!AuthState} // AuthState가 false일 때 버튼을 비활성화
+        >
+          <Typography variant="h6" sx={{fontFamily: 'Pretendard-Bold', fontSize : '20px' }}>반납하기</Typography>        
         </Button>
-
       </SwipeableDrawer>
       <Test
         isOpen={isTestOpen}
-        onClose={() => setisTestOpen(false)}
+        onClose={() => setIsTestOpen(false)}
         onStartTest={handleStartTest}
-        />
+        setAuthState={setAuthState} // setAuthState 함수를 전달합니다.
+        ws={ws} // WebSocket 정보를 전달합니다.
+        handleWebSocketMessage={handleWebSocketMessage} // WebSocket 메시지 처리 함수를 전달합니다.
+      />
     </div>
   );
 }
